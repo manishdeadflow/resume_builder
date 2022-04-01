2c8380b55f620f4c48b90bdd0e41e832c3070820
manishdeadflow <manishdeadflow@gmail.com>
2022-04-02 01:17:39 +0530

    adding user

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 const express = require("express")
+require('./db/mongodb.js')
+const USER= require('./models/users')
 
 const PORT = process.env.PORT | 3000
 
 const app = express()
+
+app.use(express.json());
+
+
+app.route('/api/v1/user').get(async (req,res) => {
+    const user=new USER(req.body)
+    await user.save()
+    res.send()
+})
 
 app.listen(PORT,()=>{
   console.log(`sever is up onthe port ${PORT}`)}
---
+++ src/models/users.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const user=mongoose.model('USER',{
+  name:{
+    type:String
+  }
+})
+
+module.exports = user ;
