3b1ab05af79b89dc4463cd39764ebcf2b72f5441
anshul21192 <83779699+anshul21192@users.noreply.github.com>
2022-04-03 01:55:43 +0530

    completed-1

--- src/models/users.js
+++ src/models/users.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator=require("validator");
 
+
 const user=mongoose.model('USER',{
   name:{
     type:String,
@@ -9,14 +10,30 @@ const user=mongoose.model('USER',{
   },
   email:{
     type:String,
-    require:true,
+    required:true,
     unique:true,
     trim:true,
     validate(val){
       if(!validator.isEmail(val)) throw new Error("invaild email");
     },
     lowercase:true
+  },
+  phoneNo:{
+    type:String,
+    required:true,
+    validate(val){
+      if(!validator.isMobilePhone(val)) throw new Error("Phone No.invaild");
+  }
+  },
+  state:{
+    type:String,
+    required:true,
+  },
+  city:{
+    type:String,
+    required:true,
   }
 });
+
 
 module.exports = user;
